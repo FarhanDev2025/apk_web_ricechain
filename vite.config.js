c7292a673d53aa3d97f00769ae7ae26274693dfa
Moh Farhan <moh.farhan111204@gmail.com>
2026-06-24 11:51:35 +0700

    Setting base URL untuk GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/apk_web_ricechain/',
   plugins: [
     react(),
     tailwindcss(),
   ],
 })
-
